60e38e5931a25c4ee3eb769587368eab035b3d2e
Alfiyan <152812058+realalf1@users.noreply.github.com>
2024-01-07 03:43:54 +0700

    Update ip_services.py

--- ip_services.py
+++ ip_services.py
@@ -1,46 +1,31 @@
 import requests
+from time import sleep
 
-def get_ip_public():
-    resp1   = requests.get("https://suip.biz/ip").text # show ip directly
-    # resp2 = requests.get("https://api.ipify.org?format=json").json() => using json if available
+class IPServicer():
+    
+    def __init__(self, ip_public=requests.get("https://suip.biz/ip")):
+        self.name = "IP Services for getting information about Internet Protocol."
+        self.ip_public = ip_public.text
+        
+    def checking_ip_public(self):
+        try:
+            while True:
+                print(f"IP Public: {self.ip_public}")
+                sleep(5)
+				
+        except KeyboardInterrupt:
+            print("Program interrupted by user")
 
-    return resp1
-    # return resp2["ip"]
+    def get_location(self):
+        r = requests.get(f"https://api.ipfind.com?ip={self.ip_public}") # limited to 100 requests / day or using API Key
+        # r = requests.get(f"https://ipapi.co/{self.ip_public}/json") => limited if programmed
+        
+        return r.text
 
-''' Checking IP Public every 5 seconds
-intr = 5
-
-try:
-    while True:
-        get_public_ip()
-        print (f"My public ip : {get_public_ip()}")
-        sleep(intr)
-
-except KeyboardInterrupt:
-    print("Program interrupted by user")
-'''
-
-def get_location(apikey):
-    ip_addr = get_ip_public()
-    resp1 = requests.get(f"https://api.ipfind.com?ip={ip_addr}&auth={apikey}").json()
-    # resp2 = ip/requests.get(f"https://ipapi.co/{ip_addr}/json").json() => limited if programmed
-
-    ''' Return specific datum from (resp2)
-    loc_data = {
-        "ip "   : ip_addr,
-        "city"  : resp.get("city")
-    }
-    '''
-
-    # return all data
-    return resp1
-
-def run():
-    ip_address = get_ip_public()
-    ip_location = get_location("your-api-key")
-
-    print (f"IP Public: {ip_address}")
-    print (ip_location)
+    def run(self):
+        print (f"IP Public: {self.ip_public}")
+        print (f"Location : \n{self.get_location()}")
 
 if __name__ == "__main__":
-    run()
+    ip = IPServicer()
+    ip.run()
